bd51f364b373c9470536ea43fe417b174a1dd332
wangyq <985759262@qq.com>
2023-03-03 20:09:39 +0800

    test

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"crypto/md5"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func main() {
-	fmt.Printf("hello")
+	t := time.Time{}
+	fmt.Println(t.Format(time.DateTime))
+	h := md5.New()
+	h.Write([]byte("hello"))
+	res := h.Sum(nil)
+	ss := hex.EncodeToString(res)
+	fmt.Printf("%s\n", ss)
+	ss = base64.StdEncoding.EncodeToString(res)
+	fmt.Printf("%s\n", ss)
+	res, err := bcrypt.GenerateFromPassword([]byte("abc"), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("%s, %d\n", res, len(res))
+
+	res, err = bcrypt.GenerateFromPassword([]byte("88**$/abc"), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("%s, %d\n", res, len(res))
 }
